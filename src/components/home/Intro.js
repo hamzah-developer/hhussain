a386b9efd1d99aaddcd411aa8c0d3073a5132fcd
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-01-26 00:22:07 +0000

    basic header being created

---
+++ src/components/home/Intro.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomeIntro () {
+  return(
+    <div className="text-pink">testing home intro</div>
+  )
+}
+
+export default HomeIntro;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'black' : '#000000',
+      'white' : '#FFFFFF',
+      'blue' : '#000000',
+      'purple' : '#3F0071',
+      'pink' : '#FB2576',
+      
+    },
     extend: {},
   },
   plugins: [],
