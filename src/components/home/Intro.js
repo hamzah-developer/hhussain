ff89814ff879a80ecc0e3968eb85f46ee20eab7b
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-02-01 22:01:00 +0000

    basic layout of the holding page complete

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -5,7 +5,7 @@ function HomeIntro () {
     <section className="home-intro text-white min-h-screen flex items-center text-center">
       <div className="container">
             <h2 className="heading-lg text-[#e2e8f0] mb-10 xl:mb-15">Hi, I'm Hamza Hussain</h2>
-            <h3 className="heading-xs text-[#e2e8f0]">A Junior Software Engineer at <a href="https://www.weareframework.co.uk/" className="text-[#7348E5] hover:underline">Framework Design</a></h3> 
+            <h3 className="heading-xs text-[#e2e8f0]">A Junior Software Engineer at <a href="https://www.weareframework.co.uk/" className="lg-max:block text-[#7348E5] hover:underline">Framework Design</a></h3> 
         </div>
     </section>
   )
