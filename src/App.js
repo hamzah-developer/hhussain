a7bc002444b6f94b6d0d51f7104655ff2abf5199
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-02-22 23:57:04 +0000

    removed unused file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './components/css/index.css';
-import Header from './components/Header';
 import HomeIntro from './components/home/Intro';
 
 function App() {
   return (
 
     <section>
-      {/* <Header/> */}
       <HomeIntro/>
     </section>
     
