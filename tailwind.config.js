5a58252c6933c8cb802e73f542c720401f411b29
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-02-26 18:15:47 +0000

    created basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.php',
+    './template-parts/**/*.php',
+    "./resources/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
