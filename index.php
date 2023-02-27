34ab17647302e0b48c734267135eb195af30a41e
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-02-27 23:27:42 +0000

    basic layout complete

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="stylesheet" href="./dist/output.css">
+    <link rel="stylesheet" href="./dist/output.css">
 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
 
-  <title>Hamza Hussain</title>
-</head>
-<body class="">
-  <section class="home-intro text-white min-h-screen flex items-center text-center bg-blue">
-    <div class="container">
-      <div class="row">
-        <div class="column">
-          <p class="heading-xl uppercase text-[#e2e8f0] bg-">Hamza Hussain</p> 
+    <title>Hamza Hussain</title>
+  </head>
+  
+  <body>
+    <section class="home-intro text-white min-h-screen flex items-center text-center bg-blue">
+      <div class="container">
+        <div class="row">
+          <h2 class="heading-lg text-[#e2e8f0] mb-15 xl:mb-25">Hi, I'm Hamza Hussain</h2>
+          <h3 class="copy-lg text-[#e2e8f0]">A Junior Software Engineer at <a href="https://www.weareframework.co.uk/" class="text-[#7348E5] hover:underline xl-max:block xl-max:mt-[8px]">Framework Design</a></h3> 
         </div>
-          <h2 class="copy text-[#e2e8f0] mb-10 xl:mb-15">Hi, I'm Hamza Hussain</h2>
-          <h3 class="heading-xs text-[#e2e8f0]">A Junior Software Engineer at <a href="https://www.weareframework.co.uk/" className="text-[#7348E5] hover:underline">Framework Design</a></h3> 
       </div>
-    </div>
-  </section>
-</body>
+    </section>
+  </body>
 </html>
