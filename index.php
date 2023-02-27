44cc2087d6d411e67b8187eb08cb03ff28b80efd
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-02-27 23:58:53 +0000

    sorted text issues

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <h2 class="heading-lg text-[#e2e8f0] mb-15 xl:mb-25">Hi, I'm Hamza Hussain</h2>
             <h3 class="copy-lg text-[#e2e8f0] mb-40">A Junior Software Engineer at <span class="xl:hidden"><br/></span> <a href="https://www.weareframework.co.uk/" class="text-[#7348E5] default-transition swipe-underline">Framework Design</a></h3> 
 
-            <div class="flex items-center justify-center space-x-15 md:space-x-30 xl:space-x-50">
+            <div class="flex items-center justify-center space-x-25 md:space-x-30 xl:space-x-50">
               <a href="" class="default-transition hover:text-[#7348E5]">
                 <svg class="w-25" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-github" viewBox="0 0 24 24"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/></svg>
               </a>
