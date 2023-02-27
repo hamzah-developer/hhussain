62b0f940264147e10840c268f1295df3451404e0
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-02-27 19:45:47 +0000

    basic typography set up

--- index.php
+++ index.php
@@ -4,9 +4,26 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+
+  <link rel="stylesheet" href="./dist/output.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+  <title>Hamza Hussain</title>
 </head>
-<body>
-  test
+<body class="">
+  <section class="home-intro text-white min-h-screen flex items-center text-center bg-blue">
+    <div class="container">
+      <div class="row">
+        <div class="column">
+          <p class="heading-xl uppercase text-[#e2e8f0] bg-">Hamza Hussain</p> 
+        </div>
+          <h2 class="copy text-[#e2e8f0] mb-10 xl:mb-15">Hi, I'm Hamza Hussain</h2>
+          <h3 class="heading-xs text-[#e2e8f0]">A Junior Software Engineer at <a href="https://www.weareframework.co.uk/" className="text-[#7348E5] hover:underline">Framework Design</a></h3> 
+      </div>
+    </div>
+  </section>
 </body>
 </html>
